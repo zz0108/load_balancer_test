bfd74324eec1703c4bc89a02b46eef0da4e28d33
蘇羿逢 <nn12572312@outlook.com>
2024-02-22 04:01:27 +0800

    first commit

---
+++ server/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get(int val)
+        {
+            _logger.LogInformation(val.ToString());
+
+            return Ok();
+        }
+    }
+}
